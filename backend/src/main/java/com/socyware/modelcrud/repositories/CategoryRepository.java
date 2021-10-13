933e664679be9efcf2fcf00b8459c41ee004cc80
Alan Souza <socyware@gmail.com>
2021-10-13 00:41:13 -0300

    Product repository, service, resource GET ok

--- backend/src/main/java/com/socyware/modelcrud/repositories/CategoryRepository.java
+++ backend/src/main/java/com/socyware/modelcrud/repositories/CategoryRepository.java
@@ -1,10 +1,11 @@
 package com.socyware.modelcrud.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.socyware.modelcrud.entities.Category;
 
-
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
