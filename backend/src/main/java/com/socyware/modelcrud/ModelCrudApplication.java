fd81f6ed0f0bae2407cb1e6e6e6be82bbc72f189
Alan Souza <socyware@gmail.com>
2021-10-12 22:24:20 -0300

    CRUD

---
+++ backend/src/main/java/com/socyware/modelcrud/ModelCrudApplication.java
@@ -0,0 +1,13 @@
+package com.socyware.modelcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelCrudApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/socyware/modelcrud/ModelCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socyware.modelcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
